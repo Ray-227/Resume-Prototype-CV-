ac8cd4387d925ced6ca092b8f30460242da374c2
Ray <ray-227@ya.ru>
2021-01-18 20:56:10 +0300

    Вторая задача: вторая попытка.

--- task2.py
+++ task2.py
@@ -1,26 +1,19 @@
 def odometer(oksana):
-  # S = V * T
-  if ( len(oksana) >= 2 ):
+  arr = oksana
+
+  if ( len(arr) >= 2 ):
     v = 0
-    temp = []
-
-    for i in range( len(oksana) ):
-      if ( oksana[i] % 2 == 0 ):
-
-        if (i+1 == len(oksana)-1 and oksana[i + 1] % 2 == 0):
-          temp.append( oksana[i] )
-          break
-        elif ( i == len(oksana)-1 ):
-          temp.append( oksana[i] )
-          break
-
-        v += oksana[i]
-      elif ( oksana[i] % 2 == 1 ):
-        temp.append( v * oksana[i] )
-        v = 0
-
+    t = 0
+    prevT = 0
     s = 0
-    for i in range( len(temp) ):
-      s += temp[i]
+
+    for i in range( len(arr) ):
+      if (i % 2 == 0):
+        v = arr[i]
+      elif (i % 2 == 1):
+        t = abs(arr[i] - prevT)
+        prevT = arr[i]
+        s += v * t
+        
 
     return s
